7b58d7b8236e4fde56148b43f72be0c8a45607cb
w3stling <peter.westling@gmail.com>
2018-03-18 18:04:19 +0100

    Added test cases

--- src/test/java/com/apptastic/insynsregistret/FreeTextQueryTest.java
+++ src/test/java/com/apptastic/insynsregistret/FreeTextQueryTest.java
@@ -1,11 +1,14 @@
 package com.apptastic.insynsregistret;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
 
 import org.junit.Test;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.util.Optional;
 
 
@@ -93,11 +96,20 @@ public class FreeTextQueryTest {
 
 
     @Test
-    public void noMatch() throws IOException {
-        FreeTextQuery query = FreeTextQueryBuilder.issuer("-").build();
+    public void freeTestQueryNullParams() throws IOException {
+        FreeTextQuery query = new FreeTextQuery(null, null);
+        assertEquals("", query.getUrl());
+    }
 
-        Insynsregistret ir = new Insynsregistret();
-        long resultCount = ir.search(query).count();
-        assertTrue(resultCount == 0);
+    @Test
+    public void badResponse() throws IOException {
+        BufferedReader reader = new BufferedReader(new StringReader("[\"Test1\",\"Test2\",\"Test3]"));
+
+        Insynsregistret mock = spy(Insynsregistret.class);
+        doReturn(reader).when(mock).sendRequest(anyString(), any());
+
+        FreeTextQuery query = FreeTextQueryBuilder.issuer("test").build();
+        long count = mock.search(query).count();
+        assertEquals(2, count);
     }
 }
