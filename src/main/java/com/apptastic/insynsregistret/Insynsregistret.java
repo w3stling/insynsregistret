8da1c93d9620754433b25db63c8caa25140feca0
w3stling <peter.westling@gmail.com>
2018-03-28 18:46:20 +0200

    Performance improvements

--- src/main/java/com/apptastic/insynsregistret/Insynsregistret.java
+++ src/main/java/com/apptastic/insynsregistret/Insynsregistret.java
@@ -28,9 +28,9 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
 import java.util.*;
+import java.util.function.BiConsumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Pattern;
 import java.util.stream.Stream;
 import java.util.zip.GZIPInputStream;
 
@@ -51,29 +51,6 @@ import java.util.zip.GZIPInputStream;
  * All insider trading is reported to FI, which publishes the data on a daily basis to this public database.
  */
 public class Insynsregistret {
-    private static final String[] COLUMN_PUBLICATION_DATE = {"Publicerings datum", "Publication date"};
-    private static final String[] COLUMN_ISSUER = {"Utgivare", "Issuer"};
-    private static final String[] COLUMN_LEI_CODE = {"LEI-kod", "LEI-code"};
-    private static final String[] COLUMN_NOTIFIER = {"Anmälningsskyldig", "Notifier"};
-    private static final String[] COLUMN_PERSON_DISCHARGING_MANAGERIAL_RESPONSIBILITIES = {"Person i ledande ställning", "Person discharging managerial responsibilities"};
-    private static final String[] COLUMN_POSITION = {"Befattning", "Position"};
-    private static final String[] COLUMN_IS_CLOSELY_ASSOCIATE = {"Närstående", "Closely associated"};
-    private static final String[] COLUMN_IS_AMENDMENT = {"Korrigering", "Amendment"};
-    private static final String[] COLUMN_DETAILS_OF_AMENDMENT = {"Beskrivning av korrigering", "Details of amendment"};
-    private static final String[] COLUMN_IS_INITIAL_NOTIFICATION = {"Är förstagångsrapportering", "Initial notification"};
-    private static final String[] COLUMN_IS_LINKED_TO_SHARE_OPTION_PROGRAMME = {"Är kopplad till aktieprogram", "Linked to share option programme"};
-    private static final String[] COLUMN_NATURE_OF_TRANSACTION = {"Karaktär", "Nature of transaction"};
-    private static final String[] COLUMN_INSTRUMENT = {"Instrument", "Instrument"};
-    private static final String[] COLUMN_ISIN = {"ISIN", "ISIN"};
-    private static final String[] COLUMN_TRANSACTION_DATE = {"Transaktions datum", "Transaction date"};
-    private static final String[] COLUMN_QUANTITY = {"Volym", "Volume"};
-    private static final String[] COLUMN_UNIT = {"Volymsenhet", "Unit"};
-    private static final String[] COLUMN_PRICE = {"Pris", "Price"};
-    private static final String[] COLUMN_CURRENCY = {"Valuta", "Currency"};
-    private static final String[] COLUMN_TRADING_VENUE = {"Handelsplats", "Trading venue"};
-    private static final String[] COLUMN_STATUS = {"Status", "Status"};
-
-
     /**
      * Free text search for issuer names or person discharging managerial responsibilities (PDMR).
      * {@link FreeTextQuery} object is created by {@link FreeTextQueryBuilder} class.
@@ -84,7 +61,7 @@ public class Insynsregistret {
     public Stream<String> search(FreeTextQuery query) throws IOException {
         BufferedReader response = sendRequest(query.getUrl(), Charset.forName("UTF-8"));
 
-        return parseStringArray(response);
+        return parseFreeTextQueryResponse(response);
     }
 
 
@@ -99,7 +76,7 @@ public class Insynsregistret {
         int langIndex = query.getLanguage().getIndex();
         BufferedReader response = sendRequest(query.getUrl(), Charset.forName("UTF-16LE"));
 
-        return parseTransactions(response, langIndex);
+        return parseTransactionResponse(response, langIndex);
     }
 
     /**
@@ -123,7 +100,7 @@ public class Insynsregistret {
     }
 
 
-    private Stream<String> parseStringArray(BufferedReader reader) {
+    private Stream<String> parseFreeTextQueryResponse(BufferedReader reader) {
         return reader.lines().flatMap(this::splitString).distinct();
     }
 
@@ -155,21 +132,65 @@ public class Insynsregistret {
     }
 
 
-    private Stream<Transaction> parseTransactions(BufferedReader reader, int langIndex) throws IOException {
-        Pattern pattern = Pattern.compile(";(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+    Stream<Transaction> parseTransactionResponse(BufferedReader reader, int langIndex) throws IOException {
         String header = reader.readLine();
 
         if (header == null)
             return Stream.empty();
 
-        Map<String, Integer> columnIndex = new HashMap<>();
-        String[] headerColumns = pattern.split(header);
-        parseHeaderLine(headerColumns, columnIndex);
+        String[] headerColumns = splitLine(header, 32);
+        TransactionAssigner assigner = new TransactionAssigner();
+        int nofColumns = assigner.initialize(headerColumns, langIndex);
 
+        // Read the rest of the lines after the header
         return reader.lines()
-                .map(pattern::split)
-                .map(columns -> toTransaction(columns, columnIndex, langIndex))
+                .parallel()
+                .map(l -> splitLine(l, nofColumns))
+                .map(assigner::createTransaction)
                 .filter(this::badTransactionFilter);
+    }
+
+
+    private String[] splitLine(String text, int nofColumns) {
+        int index = 0;
+        boolean found = true;
+        int extraForQuotationMark = 0;
+        int start = 0;
+        int end;
+        String[] tokens = new String[nofColumns];
+
+        while (found && index < nofColumns) {
+            end = text.indexOf(';', start);
+            found = end != -1;
+
+            if (found && text.codePointAt(start) == '"') {
+                // Data in column element is with quotation marks. Find the ending quote mark.
+                ++start;
+
+                if (start == end)
+                    end = text.indexOf(';', end + 1);
+
+                while (found && text.codePointAt(end - 1) != '"') {
+                    end = text.indexOf(';', end + 1);
+                    found = end != -1;
+                }
+
+                extraForQuotationMark = 1;
+                --end;
+            }
+
+            if (found) {
+                String token = text.substring(start, end);
+                token = token.replace("\\u0026", "&").trim();
+
+                tokens[index] = token;
+                start = end + 1 + extraForQuotationMark;
+                extraForQuotationMark = 0;
+                ++index;
+            }
+        }
+
+        return tokens;
     }
 
 
@@ -178,66 +199,132 @@ public class Insynsregistret {
     }
 
 
-    private void parseHeaderLine(String[] header, Map<String, Integer> columnIndexMap) {
-        for (int i = 0; i < header.length; ++i)
-            columnIndexMap.put(header[i].trim(), i);
-    }
+    static private class TransactionAssigner {
+        private static final String[] COLUMN_PUBLICATION_DATE = {"Publicerings datum", "Publication date"};
+        private static final String[] COLUMN_ISSUER = {"Utgivare", "Issuer"};
+        private static final String[] COLUMN_LEI_CODE = {"LEI-kod", "LEI-code"};
+        private static final String[] COLUMN_NOTIFIER = {"Anmälningsskyldig", "Notifier"};
+        private static final String[] COLUMN_PERSON_DISCHARGING_MANAGERIAL_RESPONSIBILITIES = {"Person i ledande ställning", "Person discharging managerial responsibilities"};
+        private static final String[] COLUMN_POSITION = {"Befattning", "Position"};
+        private static final String[] COLUMN_IS_CLOSELY_ASSOCIATE = {"Närstående", "Closely associated"};
+        private static final String[] COLUMN_IS_AMENDMENT = {"Korrigering", "Amendment"};
+        private static final String[] COLUMN_DETAILS_OF_AMENDMENT = {"Beskrivning av korrigering", "Details of amendment"};
+        private static final String[] COLUMN_IS_INITIAL_NOTIFICATION = {"Är förstagångsrapportering", "Initial notification"};
+        private static final String[] COLUMN_IS_LINKED_TO_SHARE_OPTION_PROGRAMME = {"Är kopplad till aktieprogram", "Linked to share option programme"};
+        private static final String[] COLUMN_NATURE_OF_TRANSACTION = {"Karaktär", "Nature of transaction"};
+        private static final String[] COLUMN_INSTRUMENT = {"Instrument", "Instrument"};
+        private static final String[] COLUMN_ISIN = {"ISIN", "ISIN"};
+        private static final String[] COLUMN_TRANSACTION_DATE = {"Transaktions datum", "Transaction date"};
+        private static final String[] COLUMN_QUANTITY = {"Volym", "Volume"};
+        private static final String[] COLUMN_UNIT = {"Volymsenhet", "Unit"};
+        private static final String[] COLUMN_PRICE = {"Pris", "Price"};
+        private static final String[] COLUMN_CURRENCY = {"Valuta", "Currency"};
+        private static final String[] COLUMN_TRADING_VENUE = {"Handelsplats", "Trading venue"};
+        private static final String[] COLUMN_STATUS = {"Status", "Status"};
+
+        private ArrayList<BiConsumer<Transaction, String>> columnLookup;
 
 
-    private Transaction toTransaction(String[] columns, Map<String, Integer> columnIndexMap, int langIndex) {
-        final Transaction transaction = new Transaction();
-
-        // Assign all transaction fields if present.
-        columnIndexMap.computeIfPresent(COLUMN_PUBLICATION_DATE[langIndex], (key, index) -> { transaction.setPublicationDate(columns[index]); return index; } );
-        columnIndexMap.computeIfPresent(COLUMN_ISSUER[langIndex], (key, index) -> { transaction.setIssuer(columns[index]); return index; } );
-        columnIndexMap.computeIfPresent(COLUMN_LEI_CODE[langIndex], (key, index) -> { transaction.setLeiCode(columns[index]); return index; } );
-        columnIndexMap.computeIfPresent(COLUMN_NOTIFIER[langIndex], (key, index) -> { transaction.setNotifier(columns[index]); return index; } );
-        columnIndexMap.computeIfPresent(COLUMN_PERSON_DISCHARGING_MANAGERIAL_RESPONSIBILITIES[langIndex], (key, index) -> { transaction.setPersonDischargingManagerialResponsibilities(columns[index]); return index; } );
-        columnIndexMap.computeIfPresent(COLUMN_POSITION[langIndex], (key, index) -> { transaction.setPosition(columns[index]); return index; } );
-        columnIndexMap.computeIfPresent(COLUMN_IS_CLOSELY_ASSOCIATE[langIndex], (key, index) -> { transaction.setCloselyAssociated(isTrue(columns[index], langIndex)); return index; } );
-        columnIndexMap.computeIfPresent(COLUMN_IS_AMENDMENT[langIndex], (key, index) -> { transaction.setAmendment(isTrue(columns[index], langIndex)); return index; } );
-        columnIndexMap.computeIfPresent(COLUMN_DETAILS_OF_AMENDMENT[langIndex], (key, index) -> { transaction.setDetailsOfAmendment(columns[index]); return index; } );
-        columnIndexMap.computeIfPresent(COLUMN_IS_INITIAL_NOTIFICATION[langIndex], (key, index) -> { transaction.setInitialNotification(isTrue(columns[index], langIndex)); return index; } );
-        columnIndexMap.computeIfPresent(COLUMN_IS_LINKED_TO_SHARE_OPTION_PROGRAMME[langIndex], (key, index) -> { transaction.setLinkedToShareOptionProgramme(isTrue(columns[index], langIndex)); return index; } );
-        columnIndexMap.computeIfPresent(COLUMN_NATURE_OF_TRANSACTION[langIndex], (key, index) -> { transaction.setNatureOfTransaction(columns[index]); return index; } );
-        columnIndexMap.computeIfPresent(COLUMN_INSTRUMENT[langIndex], (key, index) -> { transaction.setInstrument(columns[index]); return index; } );
-        columnIndexMap.computeIfPresent(COLUMN_ISIN[langIndex], (key, index) -> { transaction.setIsin(columns[index]); return index; } );
-        columnIndexMap.computeIfPresent(COLUMN_TRANSACTION_DATE[langIndex], (key, index) -> { transaction.setTransactionDate(columns[index]); return index; } );
-        columnIndexMap.computeIfPresent(COLUMN_QUANTITY[langIndex], (key, index) -> { transaction.setQuantity(parseDouble(columns[index])); return index; } );
-        columnIndexMap.computeIfPresent(COLUMN_UNIT[langIndex], (key, index) -> { transaction.setUnit(columns[index]); return index; } );
-        columnIndexMap.computeIfPresent(COLUMN_PRICE[langIndex], (key, index) -> { transaction.setPrice(parseDouble(columns[index])); return index; } );
-        columnIndexMap.computeIfPresent(COLUMN_CURRENCY[langIndex], (key, index) -> { transaction.setCurrency(columns[index]); return index; } );
-        columnIndexMap.computeIfPresent(COLUMN_TRADING_VENUE[langIndex], (key, index) -> { transaction.setTradingVenue(columns[index]); return index; } );
-        columnIndexMap.computeIfPresent(COLUMN_STATUS[langIndex], (key, index) -> { transaction.setStatus(columns[index]); return index; } );
-
-        return transaction;
-    }
-
-
-    private double parseDouble(String value) {
-        double floatNumber;
-
-        try {
-            value = value.replace(',', '.');
-            floatNumber = Double.valueOf(value);
-        }
-        catch (Exception e) {
-            Logger logger = Logger.getLogger("com.apptastic.insynsregistret");
-
-            if (logger.isLoggable(Level.WARNING))
-                logger.log(Level.WARNING, "Failed to parse double. ", e);
-
-            floatNumber = Double.NaN;
+        TransactionAssigner() {
+            columnLookup = new ArrayList<>();
         }
 
-        return floatNumber;
-    }
+
+        int initialize(String[] header, int langIndex) {
+            int i;
+
+            for (i = 0; i < header.length; ++i) {
+                String headerColumnText = header[i];
+
+                if (headerColumnText == null)
+                    break;
+
+                if (COLUMN_PUBLICATION_DATE[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, Transaction::setPublicationDate);
+                else if (COLUMN_ISSUER[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, Transaction::setIssuer);
+                else if (COLUMN_LEI_CODE[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, Transaction::setLeiCode);
+                else if (COLUMN_NOTIFIER[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, Transaction::setNotifier);
+                else if (COLUMN_PERSON_DISCHARGING_MANAGERIAL_RESPONSIBILITIES[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, Transaction::setPersonDischargingManagerialResponsibilities);
+                else if (COLUMN_POSITION[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, Transaction::setPosition);
+                else if (COLUMN_IS_CLOSELY_ASSOCIATE[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, (t, v) -> t.setCloselyAssociated(isTrue(v, langIndex)) );
+                else if (COLUMN_IS_AMENDMENT[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, (t, v) -> t.setAmendment(isTrue(v, langIndex)) );
+                else if (COLUMN_DETAILS_OF_AMENDMENT[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, Transaction::setDetailsOfAmendment);
+                else if (COLUMN_IS_INITIAL_NOTIFICATION[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, (t, v) -> t.setInitialNotification(isTrue(v, langIndex)) );
+                else if (COLUMN_IS_LINKED_TO_SHARE_OPTION_PROGRAMME[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, (t, v) -> t.setLinkedToShareOptionProgramme(isTrue(v, langIndex)) );
+                else if (COLUMN_NATURE_OF_TRANSACTION[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, Transaction::setNatureOfTransaction);
+                else if (COLUMN_INSTRUMENT[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, Transaction::setInstrument);
+                else if (COLUMN_ISIN[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, Transaction::setIsin);
+                else if (COLUMN_TRANSACTION_DATE[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, Transaction::setTransactionDate);
+                else if (COLUMN_QUANTITY[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, (t, v) -> t.setQuantity(parseDouble(v)) );
+                else if (COLUMN_UNIT[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, Transaction::setUnit);
+                else if (COLUMN_PRICE[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, (t, v) -> t.setPrice(parseDouble(v)) );
+                else if (COLUMN_CURRENCY[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, Transaction::setCurrency);
+                else if (COLUMN_TRADING_VENUE[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, Transaction::setTradingVenue);
+                else if (COLUMN_STATUS[langIndex].equals(headerColumnText))
+                    columnLookup.add(i, Transaction::setStatus);
+            }
+
+            return i;
+        }
 
 
-    private boolean isTrue(String text, int langIndex) {
-        if (langIndex == 1)
-            return "Yes".equalsIgnoreCase(text.trim());
-        else
-            return "Ja".equalsIgnoreCase(text.trim());
+        Transaction createTransaction(String[] columns) {
+            Transaction transaction = new Transaction();
+            int length = Math.min(columns.length, columnLookup.size());
+
+            for (int i = 0; i < length; ++i)
+                columnLookup.get(i).accept(transaction, columns[i]);
+
+            return transaction;
+        }
+
+
+        private double parseDouble(String value) {
+            double floatNumber;
+
+            try {
+                value = value.replace(',', '.');
+                floatNumber = Double.valueOf(value);
+            }
+            catch (Exception e) {
+                Logger logger = Logger.getLogger("com.apptastic.insynsregistret");
+
+                if (logger.isLoggable(Level.WARNING))
+                    logger.log(Level.WARNING, "Failed to parse double. ", e);
+
+                floatNumber = Double.NaN;
+            }
+
+            return floatNumber;
+        }
+
+
+        private boolean isTrue(String text, int langIndex) {
+            if (langIndex == 1)
+                return "Yes".equalsIgnoreCase(text.trim());
+            else
+                return "Ja".equalsIgnoreCase(text.trim());
+        }
+
     }
+
 }
