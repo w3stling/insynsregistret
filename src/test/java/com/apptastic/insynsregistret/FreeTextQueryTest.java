a4ced65a5939e77a5e6392179502e3945c20bf99
w3stling <peter.westling@gmail.com>
2018-05-31 04:08:56 +0200

    Updated unit test

--- src/test/java/com/apptastic/insynsregistret/FreeTextQueryTest.java
+++ src/test/java/com/apptastic/insynsregistret/FreeTextQueryTest.java
@@ -23,7 +23,7 @@ public class FreeTextQueryTest {
         Optional<String> issuer = ir.search(query).findFirst();
 
         assertTrue(issuer.isPresent());
-        assertEquals("LeoVegas AB", issuer.get());
+        assertEquals("LeoVegas", issuer.get());
     }
 
 
