4f9cb96d507d43a23e28641f759245c33a953441
w3stling <peter.westling@gmail.com>
2020-10-11 16:33:25 +0200

    Test case updated

--- src/test/java/com/apptastic/insynsregistret/FreeTextQueryTest.java
+++ src/test/java/com/apptastic/insynsregistret/FreeTextQueryTest.java
@@ -62,7 +62,7 @@ public class FreeTextQueryTest {
         Optional<String> issuer = ir.search(query).findFirst();
 
         assertTrue(issuer.isPresent());
-        assertEquals("Hennes & Mauritz AB, H & M", issuer.get());
+        assertEquals("H & M Hennes & Mauritz AB", issuer.get());
     }
 
 
