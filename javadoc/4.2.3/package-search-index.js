cca89c4f8c6150a9a065e49cd76160f3f6247d05
w3stling <w3stling@users.noreply.github.com>
2024-08-17 06:50:07 +0000

    Publishing javadoc

---
+++ javadoc/4.2.3/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.apptasticsoftware.insynsregistret","l":"com.apptasticsoftware.insynsregistret"}];updateSearchResults();
